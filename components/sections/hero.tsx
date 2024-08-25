09e16424397dc9df8d04668b49c06c2cf984e219
Gabriel Sufrir <113964498+gubriyeeel@users.noreply.github.com>
2024-08-25 17:28:46 +0800

    refactor: improve responsiveness of hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -6,7 +6,7 @@ import { people } from "@/constants";
 
 export function Hero() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-[600px] text-balance text-center">
+    <section className="flex flex-col items-center justify-center h-fit pt-4 pb-32 md:pt-8 text-balance text-center">
       <p className="font-semibold py-2 px-6 border rounded-full shadow-sm">
         Meet the team
       </p>
@@ -15,13 +15,15 @@ export function Hero() {
         <TextFlip />
       </h1>
 
-      <div className="flex flex-row items-center justify-center mt-8 w-full">
-        <Tooltip items={people} />
-      </div>
+      <div className="flex flex-col md:flex-row gap-8 items-center mt-14">
+        <div className="flex items-center justify-center w-full">
+          <Tooltip items={people} />
+        </div>
 
-      <Button className="mt-10 font-bold text-xl md:text-2xl rounded-full py-8 px-8">
-        Get to know us
-      </Button>
+        <Button className="font-bold text-xl md:text-2xl rounded-full py-8 px-8">
+          Get to know us
+        </Button>
+      </div>
     </section>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,7 +90,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "flip-words": "flip-words 8s infinite",
+        "flip-words": "flip-words 10s infinite",
         "animated-gradient": "animated-gradient 6s ease infinite alternate",
       },
     },
