05f9a5c3ab5b0c9de54005005c8ac56b5c3836b5
Gabriel Sufrir <113964498+gubriyeeel@users.noreply.github.com>
2024-08-30 08:17:51 +0800

    fix: change database to readonly

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 
-const sqlite = new Database("profiles.db");
+const sqlite = new Database("profiles.db", { readonly: true });
 export const database = drizzle(sqlite);
